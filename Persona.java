046905b00d4cee99efd565159013a6dd14f2790c
Yeremy Daniel Silva Guerrero <145410510+yeremy0418@users.noreply.github.com>
2024-03-22 08:20:20 -0500

    Hola

---
+++ Persona.java
@@ -0,0 +1,12 @@
+public class Persona {
+    private String nombre;
+    private int edad;
+    private long cedula;
+    private char sexo;
+    private double peso;
+    private double altura;
+    
+    public Persona(){
+        
+    }
+}
